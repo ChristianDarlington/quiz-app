2081d8e737768ace222210b4d08a00d6b06d6d03
Christian Darlington <bocacode@bocacodes-MacBook-Air.local>
2021-09-22 16:09:48 -0400

    question forming

---
+++ QuizApp/script.js
@@ -0,0 +1,45 @@
+const quizData = [
+  {
+    question:'How old is Christian Darlington?',
+    a: '19',
+    b: '21',
+    c: '20',
+    d: '22',
+    correct: c
+  },{
+    question: 'Which NBA player nickname is the \'Diesil\'?',
+    a: 'Joel Embiid',
+    b: 'Shaq',
+    c: 'Alonzo Mourning',
+    d: 'Lebron James',
+    correct: b
+  },{
+    question: 'Which of these were the most popular programming language in 2019?',
+    a:'Java',
+    b:'Javascript',
+    c:'Python',
+    d:'C',
+    correct: a
+  },{
+    question:'What is the most popular game in 2021?',
+    a:'Fornite',
+    b:'Call of Duty Warzone',
+    c: 'Minecraft',
+    d: 'Super Smash Bros. Ultimate',
+    correct: c
+  },{
+    question: 'According to box office sales, Which movie is the best?',
+    a: 'Black Widow',
+    b: 'Shang-Chi',
+    c: 'F9',
+    d: 'Snake Eyes',
+    correct: a
+  },{
+    question: 'What year was Javascript launched?',
+    a: '2010',
+    b:'2005',
+    c:'1995',
+    d:'none of the above',
+    correct: c
+  }
+]
