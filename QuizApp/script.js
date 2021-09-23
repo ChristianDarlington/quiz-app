48229935671142c43d687cef460591ddd0a31c26
Christian Darlington <bocacode@bocacodes-MacBook-Air.local>
2021-09-23 16:39:40 -0400

    questions displayed

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -43,3 +43,39 @@ const quizData = [
     correct: c
   }
 ]
+
+const questionEl = document.getElementById
+("question")
+
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const d_text = document.getElementById("d_text")
+const submitBtn = document.getElementById("submit")
+
+let currentQuestion = 0
+
+loadQuiz()
+
+function loadQuiz(){
+const currentQuizData = quizData[currentQuestion]
+
+questionEl.innerText = currentQuizData.question
+
+a_text.innerHTML = currentQuizData.a
+b_text.innerHTML = currentQuizData.b
+c_text.innerHTML = currentQuizData.c
+d_text.innerHTML = currentQuizData.d
+  
+
+}
+
+submitBtn.addEventListener('click', () => {
+  currentQuestion++
+
+  if(currentQuestion < quizData.length){
+    loadQuiz()
+  } else {
+    alert("You finished! Now eat a nice big bowl of veggies")
+  }
+})
